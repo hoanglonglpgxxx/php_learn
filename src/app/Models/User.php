b94fc4f8b50dc15b27e6a11ec420d38462b14274
Mít <hoanglonglpgxxx@gmail.com>
2023-07-19 23:27:31 +0700

    up

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -8,7 +8,8 @@ class User extends Model
 {
     public function create(string $email, string $name, bool $isActive = true): int {
         $stmt = $this->db->prepare(
-            'INSERT INTO users(email, full_name, is_active, created_at) VALUES (?,?,?,NOW())'
+            'INSERT INTO users(email, full_name, is_active, created_at)
+                    VALUES (?,?,?,NOW())'
         );
         $stmt->execute([$email,$name, $isActive]);
 
